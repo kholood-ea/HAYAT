d35c4cc5d2c6f76cdc400d3bc0fdbb2dfee9732a
kholood-ea <kholood.eassa@dija.money>
2025-09-21 12:33:07 +0300

    fix: ambient module

--- apps/HAYAT-e2e/src/support/commands.ts
+++ apps/HAYAT-e2e/src/support/commands.ts
@@ -35,3 +35,4 @@ Cypress.Commands.add('login', (email, password) => {
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+export {}
